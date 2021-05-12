885c91241aa13b7464a22fb5b77233d2bb0c918f
ccneel <corycneel@gmail.com>
2021-05-12 14:24:22 -0500

    added component structure and added imports in App.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Header from './components/Header'
+import About from './components/About'
+import Project from './components/Project'
+import Contact from './components/Contact'
+import Resume from './components/Resume'
+import Footer from './components/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [tabs] = useState(['About', 'Portfolio', 'Contact', 'Resume']);
+  const [tabSelected, setTabSelected] = useState(tabs[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='cover-container d-flex mw-100 h-100 p-3 mx-auto flex-column'>
+      <div>
+        <Header
+          tabs={tabs}
+          tabSelected={tabSelected}
+          setTabSelected={setTabSelected}
+        ></Header>
+      </div>
+      <main className="mw-100">
+        {(() => {
+          switch(tabSelected){
+            case 'Portfolio': return (<Project />);
+            case 'Contact': return (<Contact />);
+            case 'Resume': return (<Resume />);
+            default: return (<About />)
+          }
+        })()}
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
