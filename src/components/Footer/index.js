a25d6b39d1357439b8d6c08dd6519498da886e78
ccneel <corycneel@gmail.com>
2021-05-12 15:14:36 -0500

    added footer content and social media links

---
+++ src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {GoMarkGithub} from 'react-icons/go';
+import {GrLinkedin} from 'react-icons/gr';
+import {FaInstagram} from 'react-icons/fa'
+
+function Footer () {
+    return(
+        <footer className='mt-auto text-white-50'>
+            <p>
+                <a href='https://github.com/cocobeware83'  className='text-white px-3' target='_blank' rel="noreferrer"><GoMarkGithub size={24} /></a>
+                <a href='https://www.linkedin.com/in/cory-neel-0bb1a01b9/' className='text-white' target='_blank' rel="noreferrer"><GrLinkedin size={24}/></a>
+                <a href='https://www.instagram.com/ccneel/' className='text-white px-3' target='_blank' rel="noreferrer"><FaInstagram size={24}/></a>
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
+
