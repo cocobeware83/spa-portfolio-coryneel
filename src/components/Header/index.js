f0a79c81b86fc0aaf5c8c674ca706acefd6ada45
ccneel <corycneel@gmail.com>
2021-05-12 15:03:49 -0500

    added Header and Nav content

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Nav from '../Nav'
+
+function Header (props) {
+    const {
+        tabSelected,
+        setTabSelected
+    } = props;
+    return(
+        <header data-testid="header" className="mb-auto">
+            <div>
+                <h1 className='float-md-start mb-0'>Hi My Name is Cory</h1>
+                <Nav
+                tabSelected={tabSelected}
+                setTabSelected={setTabSelected}
+                ></Nav>
+            </div> 
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Nav(props){
+    const{
+        tabSelected,
+        setTabSelected
+    } = props;
+    return(
+        <nav className='nav nav-masthead justify-content-center float-md-end'>
+            <a data-testid="about" className={`nav-link unlink ${tabSelected === 'About' && 'active'}`} aria-current='page' href="#about" onClick={() => setTabSelected('About')}>About me</a>
+            <a data-testid="portfolio" className={`nav-link unlink ${tabSelected === 'Portfolio' && 'active'}`} aria-current='page' href="#portfolio" onClick={() => setTabSelected('Portfolio')}>Portfolio</a>
+            <a data-testid="contact" className={`nav-link unlink ${tabSelected === 'Contact' && 'active'}`} aria-current='page' href="#contact" onClick={() => setTabSelected('Contact')}>Contact Me</a>
+            <a data-testid="resume" className={`nav-link unlink ${tabSelected === 'Resume' && 'active'}`} aria-current='page' href="#resume" onClick={() => setTabSelected('Resume')}>Resume</a>
+        </nav>
+    );
+};
+
+export default Nav;
